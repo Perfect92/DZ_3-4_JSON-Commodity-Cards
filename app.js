f95b82b526e6b661291a7001e9f1b63c0e0bdbed
Perfect92 <bakytbekovnurzanbek@gmail.com>
2023-01-25 09:01:38 +0300

    Товарка

---
+++ app.js
@@ -0,0 +1,30 @@
+const wrapper = document.querySelector(".wrapper")
+const btn = document.querySelector(".btn")
+
+
+
+  const req = new XMLHttpRequest 
+  req.open("GET","data.json")
+  req.setRequestHeader("Content-type", "application/json")
+  req.send()
+  
+  req.addEventListener("load",()=> {
+    console.log(JSON.parse(req.response))
+    const data = JSON.parse(req.response)
+    
+
+    console.log(data.results.img)  
+
+     data.results.map((item)=>{
+      console.log(item)   
+        const block = document.createElement("div")
+          // const img = document.createElement("img")
+    //   // let img = ["./img/наушники.jpg","./img/кросовки.jpg","./img/смарт-часы.jpg","./img/смартфон.jpg","./img/самакат.jpg","./img/худи.jpg"]
+        block.style.height = "250px"
+        block.style.width = "250px"
+        block.style.marginTop = "10px"
+       
+        block.innerHTML = '<img src="'+item.img +'">'+  item.name +"<br>" + "Цена: "  + item.price +" coм"  
+        wrapper.append(block)
+      })
+    })
